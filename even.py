8e8bc913c886d23893ac38dc06f628e7289c8fd5
Chinmayi-2005 <vellanichinmayi@gmail.com>
2026-06-13 19:21:59 +0530

    first commit

---
+++ even.py
@@ -0,0 +1,32 @@
+class node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+class linkedlist:
+    def __init__(self):
+        self.head=None
+    def appened(self,data):
+        new_node=node(data)
+        if self.head is None:
+            self.head=new_node
+            return
+        temp=self.head
+        while temp.next!=None:
+            temp=temp.next   
+        temp.next=new_node
+    def count(self):
+        e=0
+        temp=self.head
+        while temp!=None:
+            
+            c=temp.data
+            if c%2==0:
+                e+=temp.data
+            temp=temp.next
+            
+        print(e)
+
+l=linkedlist()
+for i in range(6):
+    l.appened(i)
+l.count()
